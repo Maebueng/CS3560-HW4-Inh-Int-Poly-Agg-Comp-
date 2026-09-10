c1ea6c85759746988c028d1fbca825cbbf2244f5
Maebueng <maebueng@cpp.edu>
2026-09-09 23:19:19 -0700

    Initial commit with Employee.java superclass

---
+++ Employee.java
@@ -0,0 +1,47 @@
+public class Employee {
+    // Fields
+    private String firstName;
+    private String lastName;
+    private String socialSecurity;
+
+    // No arguments passed
+    public Employee() {
+        this.firstName = "John";
+        this.lastName = "Doe";
+        this.socialSecurity = "123-45-7890";
+    }
+    
+    // Default Constructor
+    public Employee(String firstName, String lastName, String socialSecurity) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurity = socialSecurity;
+    } 
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSocialSecurity() {
+        return socialSecurity
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSocialSecurity(String socialSecurity) {
+        this.socialSecurity = socialSecurity;
+    }
+
+}
